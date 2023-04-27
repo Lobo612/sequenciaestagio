4ad0ad8dd8866f918e1c89431ca878b7a7c0111f
Elian Rodrigues Ribeiro <elianrodriguesribeiro@gmail.com>
2023-04-27 16:51:38 -0300

    Envio da resposta

---
+++ Sequência.py
@@ -0,0 +1,13 @@
+def sequencia():
+    indice = 13
+    soma = 0
+    k = 0
+
+    while k < indice:
+        k = k + 1
+        soma = soma + k
+    
+    print(soma)
+
+if __name__ == "__main__":
+    sequencia()
